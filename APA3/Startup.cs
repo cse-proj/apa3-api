e26f25e6394ff12061b0936e94f2392573cafc43
Daniel Adu-Djan <adudjan.danieladd@gmail.com>
2020-11-03 23:09:25 -0500

    swagger

--- APA3/Startup.cs
+++ APA3/Startup.cs
@@ -41,11 +41,23 @@ namespace APA3
             services.AddControllers();
 
             services.AddHealthChecks();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "APA3 API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
